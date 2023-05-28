c0323d4326049f7babd44cb0810bb1553d171c20
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-05-28 12:27:17 +0400

    update arg names in tokens' constructor

--- contracts/Celestia.sol
+++ contracts/Celestia.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Celestia is ERC20 {
-    constructor(uint _totalSupply) ERC20("Celestia", "CEL") {
-        _mint(msg.sender, _totalSupply);
+    constructor(uint _initialSupply) ERC20("Celestia", "CEL") {
+        _mint(msg.sender, _initialSupply);
     }
 }
