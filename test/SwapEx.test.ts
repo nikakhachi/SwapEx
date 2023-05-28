63797896efd5b2453282d9db16f1aefe57814882
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-05-28 13:08:44 +0400

    tests for faucet interaction

--- test/SwapEx.test.ts
+++ test/SwapEx.test.ts
@@ -10,7 +10,7 @@ describe("Lock", function () {
   const CELESTIA_AMOUNT_PER_WITHDRAW = 10;
   const LUMINA_FAUCET_AMOUNT = 500;
   const LUMINA_AMOUNT_PER_WITHDRAW = 50;
-  const FAUCET_COOLDOWN = 60; // 60 seconds
+  const FAUCET_COOLDOWN = 10; // 10 seconds
 
   async function deployOneYearLockFixture() {
     const [owner, otherAccount] = await ethers.getSigners();
@@ -118,6 +118,40 @@ describe("Lock", function () {
       expect(await swapEx.reserve1()).to.eq(0);
 
       expect(Number(ethers.utils.formatUnits(await swapEx.balanceOf(owner.address)))).to.eq(0);
+    });
+  });
+  describe("Faucet", async function () {
+    it("Should Withdraw", async function () {
+      const { celestiaFaucet, luminaFaucet, celestia, lumina, otherAccount } = await loadFixture(deployOneYearLockFixture);
+
+      await celestiaFaucet.connect(otherAccount).withdraw();
+      await luminaFaucet.connect(otherAccount).withdraw();
+
+      expect(Number(ethers.utils.formatUnits(await celestia.balanceOf(otherAccount.address)))).to.eq(CELESTIA_AMOUNT_PER_WITHDRAW);
+      expect(Number(ethers.utils.formatUnits(await lumina.balanceOf(otherAccount.address)))).to.eq(LUMINA_AMOUNT_PER_WITHDRAW);
+    });
+    it("Should have cooldown", async function () {
+      const { celestiaFaucet, luminaFaucet, otherAccount } = await loadFixture(deployOneYearLockFixture);
+
+      await celestiaFaucet.connect(otherAccount).withdraw();
+      await luminaFaucet.connect(otherAccount).withdraw();
+
+      await expect(celestiaFaucet.connect(otherAccount).withdraw()).to.revertedWithCustomError(celestiaFaucet, "TooManyRequests");
+      await expect(luminaFaucet.connect(otherAccount).withdraw()).to.revertedWithCustomError(luminaFaucet, "TooManyRequests");
+    });
+    it("Should withdraw after cooldown", async function () {
+      const { celestiaFaucet, luminaFaucet, celestia, lumina, otherAccount } = await loadFixture(deployOneYearLockFixture);
+
+      await celestiaFaucet.connect(otherAccount).withdraw();
+      await luminaFaucet.connect(otherAccount).withdraw();
+
+      await new Promise((res) => setTimeout(res, FAUCET_COOLDOWN * 1000));
+
+      await celestiaFaucet.connect(otherAccount).withdraw();
+      await luminaFaucet.connect(otherAccount).withdraw();
+
+      expect(Number(ethers.utils.formatUnits(await celestia.balanceOf(otherAccount.address)))).to.eq(CELESTIA_AMOUNT_PER_WITHDRAW * 2);
+      expect(Number(ethers.utils.formatUnits(await lumina.balanceOf(otherAccount.address)))).to.eq(LUMINA_AMOUNT_PER_WITHDRAW * 2);
     });
   });
 });
