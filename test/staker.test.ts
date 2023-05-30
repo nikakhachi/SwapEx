75e81888579b2988759d8c4d96180e599b538cda
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-05-30 23:52:19 +0400

    adapt tests to updated staker contract

--- test/staker.test.ts
+++ test/staker.test.ts
@@ -6,7 +6,7 @@ const TOTAL_REWARDS = 1000;
 const DEFAULT_DURATION = 10;
 
 const deployStakerFixture = async () => {
-  const [owner, otherAccount] = await ethers.getSigners();
+  const [owner, user1, user2, user3] = await ethers.getSigners();
 
   const Celestia = await ethers.getContractFactory("Token");
   const celestia = await Celestia.deploy("Celestia", "CEL", ethers.utils.parseUnits(String(TOTAL_REWARDS)));
@@ -14,7 +14,7 @@ const deployStakerFixture = async () => {
   const Staker = await ethers.getContractFactory("Staker");
   const staker = await Staker.deploy(celestia.address);
 
-  return { staker, celestia, owner, otherAccount };
+  return { staker, celestia, owner, user1, user2, user3 };
 };
 
 describe("Staker", async function () {
@@ -26,7 +26,6 @@ describe("Staker", async function () {
     const blockTimestamp = (await ethers.provider.getBlock(tx.blockHash as string)).timestamp;
 
     expect(await staker.duration()).to.eq(DEFAULT_DURATION);
-    expect(await staker.rewardsSupply()).to.eq(ethers.utils.parseUnits(String(TOTAL_REWARDS)));
     expect(await staker.rewardRate()).to.eq(ethers.utils.parseUnits(String(TOTAL_REWARDS)).div(DEFAULT_DURATION));
     expect((await staker.lastUpdateTime()).toNumber()).to.eq(blockTimestamp);
     expect((await staker.finishAt()).toNumber()).to.eq(blockTimestamp + DEFAULT_DURATION);
@@ -36,19 +35,24 @@ describe("Staker", async function () {
     await celestia.transfer(staker.address, ethers.utils.parseUnits(String(TOTAL_REWARDS - 1)));
     await expect(staker.setRewards(ethers.utils.parseUnits(String(TOTAL_REWARDS)), DEFAULT_DURATION)).reverted;
   });
-  it("Should Get 100% Rewards", async function () {
+  it("Staking and Withdrawing (1 staker only)", async function () {
     const DURATION = 2;
-    const { staker, celestia, owner, otherAccount } = await loadFixture(deployStakerFixture);
+    const { staker, celestia, user1 } = await loadFixture(deployStakerFixture);
     await celestia.transfer(staker.address, ethers.utils.parseUnits(String(TOTAL_REWARDS)));
-    await staker.setRewards(ethers.utils.parseUnits(String(TOTAL_REWARDS)), DURATION);
+    const tx1 = await staker.setRewards(ethers.utils.parseUnits(String(TOTAL_REWARDS)), DURATION);
+    const setRewardsTimestamp = (await ethers.provider.getBlock(tx1.blockHash as string)).timestamp;
 
-    await staker.connect(otherAccount).stake({ value: ethers.utils.parseEther("1") });
+    const tx2 = await staker.connect(user1).stake({ value: ethers.utils.parseEther("1") });
+    const stakeTimestamp = (await ethers.provider.getBlock(tx2.blockHash as string)).timestamp;
+    const stakedDurationInRewardsInterval = setRewardsTimestamp + DURATION - stakeTimestamp;
 
     await new Promise((res) => setTimeout(res, DURATION * 1000));
 
-    await staker.connect(otherAccount).withdraw(ethers.utils.parseEther("1"));
-    await staker.connect(otherAccount).getRewards();
+    await staker.connect(user1).withdraw();
+    await staker.connect(user1).getRewards();
 
-    expect(Number(ethers.utils.formatEther(await celestia.balanceOf(otherAccount.address)))).to.eq(TOTAL_REWARDS);
+    expect(Number(ethers.utils.formatEther(await celestia.balanceOf(user1.address)))).to.eq(
+      (TOTAL_REWARDS / DURATION) * stakedDurationInRewardsInterval
+    );
   });
 });
