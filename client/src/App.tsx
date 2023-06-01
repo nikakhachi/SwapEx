21deb57c13978b31eadea3b5435aeb73eb2786cd
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-06-01 22:14:45 +0400

    mui tabs in main page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,15 +5,43 @@ import { Liquidity } from "./components/Liquidity";
 import { Swap } from "./components/Swap";
 import { Faucet } from "./components/Faucet";
 import { Staker } from "./components/Staker";
+import { Tabs, Tab } from "@mui/material";
+import { useState } from "react";
+
+enum TabEnum {
+  SWAP,
+  LIQUIDITY,
+  FAUCET,
+  STAKER,
+}
 
 function App() {
+  const [tab, setTab] = useState(TabEnum.SWAP);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: TabEnum) => {
+    setTab(newValue);
+  };
+
   return (
     <div>
-      <ConnectButton />
-      <Faucet />
-      <Swap />
-      <Liquidity />
-      <Staker />
+      <div style={{ width: "100%", display: "flex", justifyContent: "center", margin: "2rem 0 5rem 0" }}>
+        <ConnectButton />
+      </div>
+      <Tabs value={tab} onChange={handleChange} centered>
+        <Tab value={TabEnum.SWAP} label="Swap" />
+        <Tab value={TabEnum.LIQUIDITY} label="Liquidity" />
+        <Tab value={TabEnum.FAUCET} label="Faucet" />
+        <Tab value={TabEnum.STAKER} label="Staker" />
+      </Tabs>
+      {tab === TabEnum.SWAP ? (
+        <Swap />
+      ) : tab === TabEnum.LIQUIDITY ? (
+        <Liquidity />
+      ) : tab === TabEnum.FAUCET ? (
+        <Faucet />
+      ) : tab === TabEnum.STAKER ? (
+        <Staker />
+      ) : null}
     </div>
   );
 }
