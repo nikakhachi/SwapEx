290df714a85db4c1ba2d1b13bfd77066ea0003e4
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-06-12 13:37:22 +0400

    update contract addresses in client

--- client/src/contracts/swapEx.ts
+++ client/src/contracts/swapEx.ts
@@ -1,6 +1,6 @@
 export const SWAPEX_ADDRESS = import.meta.env.DEV
   ? "0x0165878a594ca255338adfa4d48449f69242eb8f"
-  : "0x922A6831E904788cf4dB10134aFB4E910Df4E395";
+  : "0x72d79543acF97aF49a33E06fFF52a754C9d895AC";
 export const SWAPEX_ABI = [
   {
     inputs: [
